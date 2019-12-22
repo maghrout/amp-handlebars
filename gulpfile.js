83c5090cb60105be01934918742474dc17ddcf3c
Moj <maghrout@googlemail.com>
2019-12-22 11:58:14 +0000

    feat: refactor styleguide page

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,67 @@
-const { series, dest, src, watch } = require("gulp");
-const handlebars = require("gulp-compile-handlebars");
-const rename = require("gulp-rename");
-const data = require("gulp-data");
-const del = require("del");
-const path = require("path");
-const htmlInlineAutoprefixer = require("gulp-inline-autoprefixer");
-const cleanCSS = require("gulp-clean-css");
+const { series, dest, src, watch } = require('gulp');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+const data = require('gulp-data');
+const del = require('del');
+const path = require('path');
+const htmlInlineAutoprefixer = require('gulp-inline-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
-const gtHandlebars = function() {
-  var templateData = {},
-    options = {
-      batch: ["./src/partials"],
-      helpers: {
-        toLowerCase: function(str) {
-          return str.toLowerCase();
-        },
-        increment: function(initial) {
-          return initial + 1;
-        },
-        hideIfCountdownExpired: function(endDate) {
-          const currentDate = new Date();
-          const expiryDate = new Date(endDate);
-          const diff = expiryDate - currentDate;
+const gtHandlebars = function () {
+    var templateData = {},
+        options = {
+            batch: ['./src/partials'],
+            helpers: {
+                toLowerCase: function (str) {
+                    return str.toLowerCase();
+                },
+                increment: function (initial) {
+                    return initial + 1;
+                },
+                hideIfCountdownExpired: function (endDate) {
+                    const currentDate = new Date();
+                    const expiryDate = new Date(endDate);
+                    const diff = expiryDate - currentDate;
 
-          const hasExpired = diff > 0 ? "" : "countdown--expired";
+                    const hasExpired = diff > 0 ? '' : 'countdown--expired';
 
-          return hasExpired;
+                    return hasExpired;
+                }
+            }
         }
-      }
-    };
 
-  return src("./src/pages/*.hbs")
-    .pipe(
-      data(function(file) {
-        return require(`./src/data/${path.basename(file.path, ".hbs")}.json`);
-      })
-    )
-    .pipe(handlebars(templateData, options))
-    .pipe(
-      rename(function(path) {
-        path.extname = ".html";
-      })
-    )
-    .pipe(dest("dist"));
+    return src('./src/pages/*.hbs')
+        .pipe(data(function (file) {
+            return require(`./src/data/${path.basename(file.path, '.hbs')}.json`)
+        }))
+        .pipe(handlebars(templateData, options))
+        .pipe(rename(function (path) {
+            path.extname = ".html";
+        }))
+        .pipe(dest('dist'));
 };
 
-const gtHtmlAutoprefix = function() {
-  return src("./partials/styles/*.hbs")
-    .pipe(htmlInlineAutoprefixer({ browsers: ["ie11"] }, {}, {}))
-    .pipe(dest("./partials/styles/"));
+const gtHtmlAutoprefix = function () {
+    return src('./partials/styles/*.hbs')
+        .pipe(htmlInlineAutoprefixer({ 'browsers': ['ie11'] }, {}, {}))
+        .pipe(dest('./partials/styles/'));
 };
 
-const gtCleanCss = function() {
-  return src("./src/partials/css/*.css")
-    .pipe(cleanCSS({ compatibility: "ie11" }))
-    .pipe(
-      rename(function(path) {
+const gtCleanCss = function () {
+    return src('./src/partials/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie11'}))
+    .pipe(rename(function (path) {
         path.extname = ".styles.hbs";
-      })
-    )
-    .pipe(dest("./dist/styles"));
-};
+    }))
+    .pipe(dest('./src/partials/styles'));
+}
 
-const gtWatch = function() {
-  return watch(
-    ["./src/**/*.css", "./src/data/*.json"],
-    { events: "all" },
-    series(gtClean, gtCleanCss, gtHtmlAutoprefix, gtHandlebars, gtWatch)
-  );
-};
+const gtWatch = function () {
+    return watch(['./src/**/*.css', './src/data/*.json'], { events: 'all' }, series(gtClean, gtCleanCss, gtHtmlAutoprefix, gtHandlebars, gtWatch));
+}
 
-const gtClean = function(cb) {
-  return del(["dist"]);
+const gtClean = function (cb) {
+    return del(['dist']);
 };
 
 exports.clean = gtClean;
